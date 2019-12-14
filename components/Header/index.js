5a4fa33a672c037018f9ba07e4c6889c03eb009f
unknown <fnumilat@gmail.com>
2019-12-14 11:30:00 -0600

    components created for header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,20 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector(".header-container")
+
+function Header() {
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+
+    header.classList.add("header");
+    title.classList.add("h1");
+    date.classList.add("span");
+    temp.classList.add("span");
+}
